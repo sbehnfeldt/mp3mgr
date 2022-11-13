debfc80369d03d2c00defc7e691538debcb62422
Stephen Behnfeldt <stephen@behnfeldt.pro>
2022-11-13 08:57:06 -0500

    Convert first route to PHP annotation.

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -5,10 +5,19 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class AppController extends AbstractController
 {
 
+    /**
+     * @return Response
+     */
+    #[Route(
+        '/',
+        name: 'homepage',
+        methods: ['GET']
+    )]
     public function index() : Response
     {
         return $this->render( 'index.html.twig' );
